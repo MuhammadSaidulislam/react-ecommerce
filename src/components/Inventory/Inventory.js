97fd1654f4c74e760ed9159564a93f5e748b22b5
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-04-04 11:36:52 +0600

    inventory

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -42,3 +42,60 @@ const Inventory = () => {
 };
 
 export default Inventory;
+
+
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>Inventory coming soon...</h1>
+        </div>
+    );
+};
+
+export default Inventory;
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>Inventory coming soon...</h1>
+        </div>
+    );
+};
+
+export default Inventory;
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>Inventory coming soon...</h1>
+        </div>
+    );
+};
+
+export default Inventory;
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>Inventory coming soon...</h1>
+        </div>
+    );
+};
+
+export default Inventory;
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>Inventory coming soon...</h1>
+        </div>
+    );
+};
+
+export default Inventory;
